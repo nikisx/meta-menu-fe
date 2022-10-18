882aaee9ffc2f166b35a981411d5714ce3e385b9
Nikolas Kostadinov <64860672+nikisx@users.noreply.github.com>
2022-10-18 21:10:48 +0300

    init

---
+++ src/components/Menu/Menu.vue
@@ -0,0 +1,37 @@
+<template>
+  <section style="text-align: center;">
+        <h1>Add items</h1>
+        <h3 @click="addCategory = true" style="cursor: pointer;">+ Add category</h3>
+        <div v-show="addCategory">
+            <input type="text">
+        </div>
+  </section>
+</template>
+
+<script>
+export default {
+    data() {
+       return {
+         categories: [],
+         addCategory: false,
+       };
+     },
+     computed:{
+     user(){
+       return this.$store.state.user;
+     }
+   },
+   created(){
+   },
+   components:{
+       
+   },
+   methods:{
+       
+   }
+}
+</script>
+
+<style>
+
+</style>
